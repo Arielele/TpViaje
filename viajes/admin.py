7f9fbb0cd5b7f088bff4c80236ee1d9e7441bd1f
Ariel <arieljbotello@gmail.com>
2022-06-08 14:50:59 -0300

    verif

--- viajes/admin.py
+++ viajes/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from viajes.models import Viaje, Destino
+from viajes.models import Viaje, Destino, Hotel
 
 # Register your models here.
 
 admin.site.register(Viaje)
 admin.site.register(Destino)
+admin.site.register(Hotel)
