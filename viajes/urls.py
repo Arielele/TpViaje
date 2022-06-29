ae47fd5d4fc3a9b7ae25ea96aae500c86ea95e71
Agustin Vigliocco <agusvigliocco@icloud.com>
2022-06-29 09:43:12 -0300

    cambio en nombres de html y urls para que quede todo en el mismo idioma

--- viajes/urls.py
+++ viajes/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from viajes.views import listar_viajes, listar_destinos, listar_hoteles, create_viaje, buscar_viajes
 
 urlpatterns = [
-    path('', listar_viajes, name = 'lista_viajes'),
-    path('destinos/', listar_destinos, name = 'lista_destinos'),
+    path('', listar_viajes, name='lista_viajes'),
+    path('destinos/', listar_destinos, name='lista_destinos'),
     path('hoteles/', listar_hoteles, name='lista_hoteles'),
-    path('create_viaje/', create_viaje, name='create_viaje'),
-    path('buscar-viajes/', buscar_viajes, name ='buscar-viajes')
+    path('crear-viajes/', create_viaje, name='create_viaje'),
+    path('buscar-viajes/', buscar_viajes, name='buscar-viajes')
 ]
--- viajes/views.py
+++ viajes/views.py
@@ -9,45 +9,48 @@ from viajes.forms import Viaje_form
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
 def listar_viajes(request):
     lista_viajes = Viaje.objects.all()
     context = {'lista_viajes': lista_viajes}
 
-    return render ( request, 'lista_viajes.html', context=context)
+    return render(request, 'lista_viajes.html', context=context)
 
 
 def listar_destinos(request):
     lista_destinos = Destino.objects.all()
     context = {'lista_destinos': lista_destinos}
 
-    return render ( request, 'lista_destinos.html', context=context)
+    return render(request, 'lista_destinos.html', context=context)
+
 
 def listar_hoteles(request):
     lista_hoteles = Hotel.objects.all()
     context = {'lista_hoteles': lista_hoteles}
-    
-    return render ( request, 'lista_hoteles.html', context=context)
+
+    return render(request, 'lista_hoteles.html', context=context)
 
 
 def create_viaje(request):
     if request.method == 'GET':
-       form= Viaje_form()
-       context = {'form': form}
-       return render( request, 'create_viaje.html', context = context )
+        form = Viaje_form()
+        context = {'form': form}
+        return render(request, 'crear-viajes.html', context=context)
     else:
-        form= Viaje_form(request.POST)
+        form = Viaje_form(request.POST)
         if form.is_valid():
             new_viaje = Viaje.objects.create(
-               precio = form.cleaned_data ['precio'],
-               destino = form.cleaned_data ['destino'],
-               fecha_salida = form.cleaned_data ['fecha_salida'],
-               fecha_regreso = form.cleaned_data ['fecha_regreso'],
+                precio=form.cleaned_data['precio'],
+                destino=form.cleaned_data['destino'],
+                fecha_salida=form.cleaned_data['fecha_salida'],
+                fecha_regreso=form.cleaned_data['fecha_regreso'],
             )
-            context = {'new_viaje' : new_viaje}
-        return render( request, 'create_viaje.html', context= context)
+            context = {'new_viaje': new_viaje}
+        return render(request, 'crear-viajes.html', context=context)
+
 
 def buscar_viajes(request):
     print(request.GET)
-    viajes = Viaje.objects.filter(destino__icontains = request.GET['buscar'])
-    context = {'viajes' : viajes}
-    return render(request, 'buscar-viajes.html', context = context)
+    viajes = Viaje.objects.filter(destino__icontains=request.GET['buscar'])
+    context = {'viajes': viajes}
+    return render(request, 'buscar-viajes.html', context=context)
